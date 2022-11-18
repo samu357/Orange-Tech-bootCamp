781257d978436ea4acff86178aa989de974219cb
samu357 <samueleduardo11@hotmail.com>
2022-11-17 23:21:37 -0400

    aula de array

---
+++ Primeiras paginas interativas com JS/arrays/aula.js
@@ -0,0 +1,24 @@
+/*const nome = 'samuel'
+
+for (let i = 0; i < nome.length; i++) {
+  console.log(nome[i]);
+}
+*/
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(10);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+  const nota = notas[i];
+  soma = soma + nota;
+}
+
+const media = soma / notas.length;
+console.log(media);
---
+++ Primeiras paginas interativas com JS/arrays/tabuada.js
@@ -0,0 +1,5 @@
+const numero = 7;
+for (let i = 1; i <= 10; i++) {
+  console.log(i * numero);
+
+}
