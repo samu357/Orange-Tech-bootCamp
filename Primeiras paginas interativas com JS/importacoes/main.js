a1cfb928a0ee67d6202bdc024ea245287518b21d
samu357 <samueleduardo11@hotmail.com>
2022-11-20 17:59:01 -0400

    aula de importações

---
+++ Primeiras paginas interativas com JS/importacoes/main.js
@@ -0,0 +1,12 @@
+const { gets, print } = require('./funcoes-auxiliares')
+
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < 5; i++) {
+  const numeroSorteado = gets();
+  if (numeroSorteado > maiorValorEncontrado) {
+    maiorValorEncontrado = numeroSorteado;
+  }
+}
+
+print(maiorValorEncontrado);
